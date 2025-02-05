1c26d052b500d24624506b0e5260ce7aff72bf25
Projecthrishi <114040477+Projecthrishi@users.noreply.github.com>
2025-02-05 22:00:22 +0530

    Update index.js

--- ASSI3/Q11/index.js
+++ ASSI3/Q11/index.js
@@ -22,23 +22,23 @@ let students = {
     }
 };
 
-// Function to calculate average
+
 function calculateAverage(students) {
     let result = {};
     
-    // Iterate over each student
+    
     for (let student in students) {
         let subjects = students[student];
         let total = Object.values(subjects).reduce((sum, score) => sum + score, 0);
         let average = total / Object.values(subjects).length;
         
-        // Store the average in the result object
+       
         result[student] = { average: average };
     }
     
     return result;
 }
 
-// Calculate and display result
+
 let averages = calculateAverage(students);
 console.log(averages);
