8f1eb03f6d81f898dc260f3b94c263107a32545b
Projecthrishi <114040477+Projecthrishi@users.noreply.github.com>
2025-02-05 21:58:55 +0530

    Update index.js

--- ASSI3/Q8/index.js
+++ ASSI3/Q8/index.js
@@ -1,12 +1,12 @@
 function repeatedDigitSum(num) {
-    // Loop until the number has only one digit
+    
     while (num >= 10) {
         num = num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
     }
     return num;
 }
 
-// Example usage
+
 console.log(repeatedDigitSum(456));  // Output: 6
 console.log(repeatedDigitSum(1234)); // Output: 1
 console.log(repeatedDigitSum(999));  // Output: 9
