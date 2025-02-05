2735ccd1b96f7a7d1ecdf805208c4d30ed014eff
Projecthrishi <114040477+Projecthrishi@users.noreply.github.com>
2025-02-05 21:58:02 +0530

    Update index.js

--- ASSI3/Q5/index.js
+++ ASSI3/Q5/index.js
@@ -2,8 +2,8 @@ function correctfn(string, wrong, correct) {
     return string.replace(new RegExp(`\\b${wrong}\\b`, 'g'), correct);
 }
 
-// Example usage:
+:
 let sentence = "I love coding in JavaScript. JavaScript is fun.";
 let correctedSentence = correctfn(sentence, "JavaScript", "TypeScript");
 
-console.log(correctedSentence);  // Output: "I love coding in TypeScript. TypeScript is fun."
+console.log(correctedSentence);  
